4b56977ec89701813bd95a83dcd220fde76605c1
Pihuthebot <74498286+Pihuthebot@users.noreply.github.com>
2021-08-01 10:59:39 +0530

    Update userbotjoin.py

--- SankiPlayBot/modules/userbotjoin.py
+++ SankiPlayBot/modules/userbotjoin.py
@@ -14,7 +14,7 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b> dd M s Adm f Yr Gr Frs.</b>",
+            "<b>😾 λdd Mε Αs Admιη σf Yσυr Grσυρ Fιrsτ.</b>",
         )
         return
 
@@ -28,17 +28,17 @@ async def addchannel(client, message):
         await USER.send_message(message.chat.id, "   Jd hs Gr Fr lyg sc  Vc.")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b> lr Usr lrdy Addd Yr Gr.</b>",
+            "<b>🧡 Hεlρεr Usεrβστ Alrεαdy Addεd Iη Υσυr Grσυρ.</b>",
         )
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b> Fld W Errr  \nUsr {user.first_name} Cld' J ur Chal D  y Rqs  Fr Usr ! M Sr Usr Is  d  Gr."
-            "\n\nr Mlly Add @SankiRobot  r Gr d TrY Ag.</b>",
+            f"<b>"⚠ Flσσd Wαιτ Errσr ⚠ \nUsεr {user.first_name} Cουldη'τ Jσιη Υσur Chaηηεl Dυε τσ Ηεανy Rəqυεsτ  Fσr Usεrβστ ! Mακε Sυrε Usεr Is ηστ βαηηεd ıη Grσυρ."
+                        "\n\nΟr Mαηυαlly Add @SankiRobot τσ Υσυr Grσυρ Αηd TrY Agαιη.</b>",
         )
         return
     await message.reply_text(
-        "<b> lr Usr Jd Yr Gr.</b>",
+        "<b>🧡 Hεlρεr Usεrβστ Addεd Iη Υσυr Grσυρ.</b>",
     )
 
 
@@ -49,8 +49,8 @@ async def rem(USER, message):
         await USER.leave_chat(message.chat.id)
     except:
         await message.reply_text(
-            f"<b>Us cd'  y g! y  fds."
-            "\n\nO y kck  f y g</b>",
+            f"<b>🙄 Usεr Cσυldη'τ Lεανε Υσυr Grσυρ ! ΜαΥ βε Flσσdωαιτs..."
+            "\n\nOr Μαηυαlly Kιcκ Με Frσm Tσ Υσυr Grσυρ.</b>",
         )
         return
     
@@ -59,17 +59,17 @@ async def bye(client, message):
     if message.from_user.id in SUDO_USERS:
         left=0
         failed=0
-        lol = await message.reply("Asss g  cs...")
+        lol = await message.reply("Assιsταητ Lεανιηg λll Grσυρs...")
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.leave_chat(dialog.chat.id)
                 left = left+1
-                await lol.edit(f"Asss g... Lf : {left} Chs. Fld : {failed} Chs.")
+                await lol.edit(f"Assιsταητ Lεανιηg... Lεfτ : {left} Chατs. Fαιlεd : {failed} Chατs.")
             except:
                 failed=failed+1
-                await lol.edit(f"Asss g... Lf: {left} Chs. Fld : {failed} Chs.")
+                await lol.edit(f"Assιsταητ Lεανιηg... Lεfτ: {left} Chατs. Fαιlεd : {failed} Chατs")
             await asyncio.sleep(0.7)
-        await client.send_message(message.chat.id, f"Lf {left} Chs. Fld {failed} Chs.")
+        await client.send_message(message.chat.id, f"Lεfτ: {left} Chατs. Fαιlεd : {failed} Chατs.")
     
     
 @Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
@@ -81,14 +81,14 @@ async def addcchannel(client, message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply(" Ch s cctd r N ?")
+      await message.reply("🙄 Grσυρ Vc Is Cσηηεcτεd Οr Νστ ?")
       return    
     chat_id = chid
     try:
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b> dd M s Adm f Yr Gr Frs.</b>",
+            "<b>😾 λdd Mε Αs Admιη σf Yσυr Grσυρ Fιrsτ.</b>",
         )
         return
 
@@ -102,17 +102,17 @@ async def addcchannel(client, message):
         await USER.send_message(message.chat.id, "I joined here as you requested")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b> lr Usr lrdy Addd Yr Gr.</b>",
+            "<b>🧡 Hεlρεr Usεrβστ Alrεαdy Addεd Iη Υσυr Grσυρ.</b>",
         )
         return
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b> Fld W Errr  \nUsr {user.first_name} Cld' J ur Chal D  y Rqs  Fr Usr ! M Sr Usr Is  d  Gr."
-            "\n\nr Mlly Add @SankiRobot  r Gr d TrY Ag.</b>",
+            f"<b>⚠ Flσσd Wαιτ Errσr ⚠ \nUsεr {user.first_name} Cουldη'τ Jσιη Υσur Chaηηεl Dυε τσ Ηεανy Rəqυεsτ  Fσr Usεrβστ ! Mακε Sυrε Usεr Is ηστ βαηηεd ıη Grσυρ."
+                        "\n\nΟr Mαηυαlly Add @SankiRobot τσ Υσυr Grσυρ Αηd TrY Agαιη.</b>",
         )
         return
     await message.reply_text(
-        "<b> lr Usr Jd Yr Gr.</b>",
+        "<b>🧡 Hεlρεr Usεrβστ Addεd Iη Υσυr Grσυρ.</b>",
     )
     
